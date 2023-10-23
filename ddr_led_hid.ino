44126461e17932cd0b26e3115fc50d83766cc995
Erich <erichnguyen25@gmail.com>
2023-10-23 12:45:59 -0500

    Example

--- ddr_led_hid.ino
+++ ddr_led_hid.ino
@@ -1,5 +1,6 @@
 #include "PluggableUSB.h"
 #include "HID.h"
+#include <FastLED.h>
 
 typedef struct {
   uint8_t brightness;
@@ -11,53 +12,110 @@ typedef struct {
   uint8_t b;
 } RGBLed;
 
+typedef struct {
+  uint8_t start; // starts at index 1 for simplicity lol
+  uint8_t end;
+} RGBPortion;
+
+typedef struct {
+  int inputPin;
+  int multiplier;
+  RGBPortion portion;
+  int frq; // vvvv   For efficiency, hard code the values
+  int numLeds; // (end-start)+1
+  int halfLidx; // ((numLeds/2)-1)+start
+  int halfRidx; // halfLidx + 1
+} AudioVisualizerStrip;
+
 // ******************************
 // EDIT THESE LINES
 
 // The single LEDs will be first in BTools
 // The RGB LEDs will come afterwards, with R/G/B individually
 #define NUMBER_OF_SINGLE 1
-#define NUMBER_OF_RGB 0
+#define NUMBER_OF_RGB 8 // portions
 
-// for this example we have just a single light attached to the inbuilt LED
-// attached to pin 13. You can add more if you want! Just update NUMBER_OF_SINGLE
-// along with this array
-int my_lights[NUMBER_OF_SINGLE] = {
-  13,
+#define NUM_LEDS 300
+#define NEOPIXEL_PIN 12
+#define NEOPIXEL_BRIGHTNESS 30
+
+CRGB led[NUM_LEDS];
+
+const int my_lights[NUMBER_OF_SINGLE] = {
+  8,
+};
+const RGBPortion portions[NUMBER_OF_RGB] = {
+  {start:1,end:3},
+  {start:4,end:6},
+  {start:7,end:8},
+  {start:10,end:11},
+  {start:20,end:24},
+  {start:25,end:29},
+  {start:30,end:34},
+  {start:35,end:39},
 };
 
-/* Important note!
- *  light_update works inside an interrupt handler. If the code inside here
- *  takes more than about 1ms, the USB stack will start breaking. Delay functions
- *  will also not work. Do not write out to Serial, neopixels, SPI etc. Just
- *  copy the data to a good place and then send it out in loop()
- *
- *  Check the "examples" folder for how to do Neopixels without breaking.
- */
+AudioVisualizerStrip visualizers[2] {
+  {
+    inputPin: 4,
+    multiplier: 3,
+    portion: {start: 40, end: 79},
+    frq: 0,
+    numLeds: 40,
+    halfLidx: 59,
+    halfRidx: 60
+  },
+  {
+    inputPin: 5,
+    multiplier: 3,
+    portion: {start: 100, end: 139},
+    frq: 0,
+    numLeds: 40,
+    halfLidx: 119,
+    halfRidx: 120
+  }
+};
 
 void light_update(SingleLED* single_leds, RGBLed* rgb_leds) {
-  for(int i = 0; i < NUMBER_OF_SINGLE; i++) {
-    // our lights use uint8_t which has a range from 0 to 255
-    // you could use a PWM pin and use analogWrite, but this example is very basic.
-    // So instead, we'll just call any value > 128 to be on
-    bool on = single_leds[i].brightness > 128;
-    // digitalWrite is very quick so we can safely call it inside the interrupt
-    digitalWrite(my_lights[i], on);
+  for(int i=0;i<NUMBER_OF_SINGLE;i++) {
+    analogWrite(my_lights[i], single_leds[i].brightness);
   }
-  for(int i = 0; i < NUMBER_OF_RGB; i++) {
-    // this example doesn't support RGB LEDs, you'll have to do it yourself
+  for(int i=0;i<NUMBER_OF_RGB;i++) {
+    RGBPortion portion = portions[i];
+    for(int j = portion.start-1; j < portion.end; j++) {
+      led[j] = CRGB(rgb_leds[i].r, rgb_leds[i].g, rgb_leds[i].b);
+    }
   }
+  // needs_update = true;
 }
 
 void setup() {
-  // setup the LEDs to be outputs
-  for(int i = 0; i < NUMBER_OF_SINGLE; i++) {
-    pinMode(my_lights[i], OUTPUT);
+  FastLED.addLeds<NEOPIXEL, NEOPIXEL_PIN>(led, NUM_LEDS);
+  FastLED.setBrightness(NEOPIXEL_BRIGHTNESS);
+  for(int i=0;i<2;i++) {
+    pinMode(visualizers[i].inputPin, INPUT);
   }
 }
 
 void loop() {
-  // USB module does the hard work
+  for(int i=0;i<2;i++) {
+    AudioVisualizerStrip strip = visualizers[i];
+    // Uncomment these lines if you don't want to hard code the values and make it less efficient lol
+    // strip.numLeds = (strip.portion.end - strip.portion.start) + 1;
+    // strip.halfLidx = int((strip.numLeds*.5)-1) + strip.portion.start;
+    // strip.halfRidx = int(strip.numLeds*.5) + strip.portion.start;
+    strip.frq = analogRead(strip.inputPin)*strip.multiplier;
+    CHSV col = CHSV(strip.frq%160, 255, min(strip.frq, 255));
+    led[strip.halfLidx] = col;
+    led[strip.halfRidx] = col;
+    for(int j=strip.portion.start; j<=strip.halfLidx-1; j++) {
+      const int idx1 = strip.numLeds-j+(strip.portion.start*2)-1;
+      led[j] = led[j+1];
+      led[idx1] = led[idx1-1];
+    }
+  }
+  delay(17);
+  FastLED.show();
 }
 
 // ******************************
@@ -103,7 +161,7 @@ static const uint8_t PROGMEM _hidReportLEDs[] = {
 class HIDLED_ : public PluggableUSBModule {
 
   uint8_t epType[1];
-
+  
   public:
     HIDLED_(void) : PluggableUSBModule(1, 1, epType) {
       epType[0] = EP_TYPE_INTERRUPT_IN;
@@ -119,33 +177,33 @@ class HIDLED_ : public PluggableUSBModule {
       };
       return USB_SendControl(0, &hidInterface, sizeof(hidInterface));
     }
-
+    
     int getDescriptor(USBSetup& setup)
     {
       // Check if this is a HID Class Descriptor request
       if (setup.bmRequestType != REQUEST_DEVICETOHOST_STANDARD_INTERFACE) { return 0; }
       if (setup.wValueH != HID_REPORT_DESCRIPTOR_TYPE) { return 0; }
-
+    
       // In a HID Class Descriptor wIndex contains the interface number
       if (setup.wIndex != pluggedInterface) { return 0; }
-
+    
       return USB_SendControl(TRANSFER_PGM, _hidReportLEDs, sizeof(_hidReportLEDs));
     }
-
+    
     bool setup(USBSetup& setup)
     {
       if (pluggedInterface != setup.wIndex) {
         return false;
       }
-
+    
       uint8_t request = setup.bRequest;
       uint8_t requestType = setup.bmRequestType;
-
+    
       if (requestType == REQUEST_DEVICETOHOST_CLASS_INTERFACE)
       {
         return true;
       }
-
+    
       if (requestType == REQUEST_HOSTTODEVICE_CLASS_INTERFACE) {
         if (request == HID_SET_REPORT) {
           if(setup.wValueH == HID_REPORT_TYPE_OUTPUT && setup.wLength == NUMBER_OF_LIGHTS){
@@ -155,7 +213,7 @@ class HIDLED_ : public PluggableUSBModule {
           }
         }
       }
-
+    
       return false;
     }
 };
